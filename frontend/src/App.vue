0b7bc116f7e2ad36cf5f06df0ff1f17bf4e6553b
j-ordep <joaoppinheiro2005@gmail.com>
2025-08-19 10:09:41 -0300

    fix: adiciona frontend como pasta normal

---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view/>
+    </div>
+    <Footer/>
+  </div>  
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  name: "App",
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "make your burger"
+    }
+  },
+
+  components: {
+    Navbar,
+    Footer,
+  }
+}
+</script>
+
+<style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px; /* problema ta aqui */
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-container">
+    <h1>aqui é o home brother</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
